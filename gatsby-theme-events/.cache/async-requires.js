04a4bb23f17cf3738709275ce40d8cd09e01d487
LadyKerr <kadiekerr@gmail.com>
2019-07-23 18:20:23 -0400

    pages rendered on the screen!

--- gatsby-theme-events/.cache/async-requires.js
+++ gatsby-theme-events/.cache/async-requires.js
@@ -2,6 +2,8 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
+  "component---src-templates-events-js": () => import("C:\\Users\\Kadie K\\lambda\\PERSONAL-PROJECTS\\First-Gatsby-Site\\gatsby-theme-events\\src\\templates\\events.js" /* webpackChunkName: "component---src-templates-events-js" */),
+  "component---src-templates-event-js": () => import("C:\\Users\\Kadie K\\lambda\\PERSONAL-PROJECTS\\First-Gatsby-Site\\gatsby-theme-events\\src\\templates\\event.js" /* webpackChunkName: "component---src-templates-event-js" */),
   "component---cache-dev-404-page-js": () => import("C:\\Users\\Kadie K\\lambda\\PERSONAL-PROJECTS\\First-Gatsby-Site\\gatsby-theme-events\\.cache\\dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */)
 }
 
--- gatsby-theme-events/.cache/sync-requires.js
+++ gatsby-theme-events/.cache/sync-requires.js
@@ -5,6 +5,8 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
+  "component---src-templates-events-js": hot(preferDefault(require("C:\\Users\\Kadie K\\lambda\\PERSONAL-PROJECTS\\First-Gatsby-Site\\gatsby-theme-events\\src\\templates\\events.js"))),
+  "component---src-templates-event-js": hot(preferDefault(require("C:\\Users\\Kadie K\\lambda\\PERSONAL-PROJECTS\\First-Gatsby-Site\\gatsby-theme-events\\src\\templates\\event.js"))),
   "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Kadie K\\lambda\\PERSONAL-PROJECTS\\First-Gatsby-Site\\gatsby-theme-events\\.cache\\dev-404-page.js")))
 }
 
